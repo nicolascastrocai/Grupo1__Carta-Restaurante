3762c4de1f085f8e7068198987b1d2b504bff50e
unknown <nicolascastrocai@gmail.com>
2022-07-26 20:02:35 -0300

    Retoques terminados

--- js/dinamismo.js
+++ js/dinamismo.js
@@ -7,11 +7,12 @@ function mostrarLinkInicio() {
     for (let index = 0; index < linkInicio.length; index++) {
         let elemento=linkInicio[index]
         link[index].innerText=elemento.Titulo
-    
+       
         
     }
 }
-mostrarLinkInicio()
 
-let menu=document.querySelectorAll('.contenedorMenu1 div')
-console.log(menu);
+
+
+
+mostrarLinkInicio()
